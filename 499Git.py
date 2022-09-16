f1f8f08ee53f703a4f3fbd37a3823cd5fd33d641
rvanoerle <75710067+rvanoerle@users.noreply.github.com>
2022-09-16 15:13:16 -0700

    First sorting feature
    Added the first feature which is sorting the list in ascending order.

--- 499Git.py
+++ 499Git.py
@@ -1,6 +1,35 @@
 
-def main():
 
-    toSort = [0,7,4,5,8,3,2,9,1,6]
+toSort = [0,7,4,5,8,3,2,9,1,6]
+
+def asc(list):
+   ascOrder = []
+   temp = list.copy()
+   while temp:
+      min = temp[0]
+      for x in temp:
+         if(x < min):
+            min = x
+      ascOrder.append(min)
+      temp.remove(min)
+        
+   print(ascOrder)
+   return ascOrder
+
+def ascTest(list):
+   temp = 0
+   for x in range(len(list)-1):
+      if list[x] > list[x+1]:
+         print("Test failed")
+         return
+   print("Test Passed")
+
+print(toSort)
+ascTest(toSort)
+ascList = asc(toSort)
+ascTest(ascList)
+
+
+
 
 
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Cosc499individual
+# Ryder Van Oerle 10817427
+
 Github Exercise
+
+The goal of this program is to write two different sorting algorithms for a list in python.
+The first feature will be sorting in ascending order and the second will be in descending order.
+
+To use the first feature all you must do is clone this repository onto your location machine, make sure you have 
+python installed, open the file into your preferred IDE and then hit run. 
+If done correctly this is the output you should recieve:
+
+[0, 7, 4, 5, 8, 3, 2, 9, 1, 6]
+Test failed
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+Test Passed
+
